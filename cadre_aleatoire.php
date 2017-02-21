b88780693330d21ee1e0fb2d71467cf179f99436
ohpla <oceane-j@laposte.net>
2017-02-21 19:49:08 +0100

    Create cadre_aleatoire.php

---
+++ cadre_aleatoire.php
@@ -0,0 +1,11 @@
+<?php
+header('Cache-Control: no-cache');
+$imgs = array(
+                'http://static.tumblr.com/eznt7u3/vcIolql62/cadre01.png',
+                'http://static.tumblr.com/eznt7u3/j8iolqlt3/cadre02.png',
+                'http://static.tumblr.com/eznt7u3/NYHolqltc/cadre03.png',
+                'ihttp://static.tumblr.com/eznt7u3/MvKolqltl/cadre04.png'
+        );
+$img_id = mt_rand(0,(count($imgs)-1));
+header('Location: '.$imgs[$img_id]);
+?>
